2b338e518742180e062cbe17a5ec986268fa5941
oreum <1117aaa@naver.com>
2015-06-15 11:16:13 -0700

    m

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,28 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get signup" do
-    get :signup
-    assert_response :success
-  end
-
-  test "should get signup_complete" do
-    get :signup_complete
-    assert_response :success
-  end
-
-  test "should get login" do
-    get :login
-    assert_response :success
-  end
-
-  test "should get login_complete" do
-    get :login_complete
-    assert_response :success
-  end
-
-  test "should get logout_complete" do
-    get :logout_complete
+  test "should get find" do
+    get :find
     assert_response :success
   end
 
